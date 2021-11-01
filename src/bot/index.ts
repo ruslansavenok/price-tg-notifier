cba7eb9a96aaafd399d9c94e2e90346bc4dea342
Ruslan Savenok <ruslan.savenok@gmail.com>
2021-11-01 10:17:52 +0100

    Add error handler

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,4 +1,4 @@
-import { Bot } from 'grammy';
+import { Bot, GrammyError, HttpError } from 'grammy';
 import { TELEGRAM_BOT_TOKEN } from '../../config';
 import loginHandler from './loginHandler';
 import subscribeHandler from './subscribeHandler';
@@ -11,6 +11,19 @@ bot.use(loginHandler('login'));
 bot.use(subscribeHandler('sub'));
 bot.use(unsubscribeHandler('unsub'));
 bot.use(listHandler('list'));
+
+bot.catch(err => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
 
 async function startBot() {
   await bot.api.setMyCommands([
