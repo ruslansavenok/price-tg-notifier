b6cc06ba466c3b6da672a6e2dafca7133bfa8e51
Ruslan Savenok <ruslan.savenok@gmail.com>
2021-11-21 20:27:28 +0100

    parse listings

--- src/parser/parseItemPage.ts
+++ src/parser/parseItemPage.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import { Iconv } from 'iconv';
-import cherio from 'cheerio';
+import cherio, { Cheerio, Element } from 'cheerio';
 import { DATASOURCE_HOSTNAME } from '../../config';
+
+export interface IItemListing {
+  id: number;
+  sellerName: string;
+  sellerLocation: string;
+  price: number;
+  addetAt: number;
+  amount?: number;
+  enchantmentLvl?: number;
+}
 
 export interface IParseItemInfo {
   title: string;
-}
-
-function itemUrl(itemId: number) {
-  return `http://${DATASOURCE_HOSTNAME}/?c=market&a=item&id=${itemId}`;
+  listings: IItemListing[];
 }
 
 async function fetchPageHtml(url: string): Promise<string> {
@@ -25,21 +32,77 @@ async function fetchPageHtml(url: string): Promise<string> {
   return page.data;
 }
 
-async function parseItemPage(itemId: number): Promise<IParseItemInfo> {
-  const url = itemUrl(itemId);
-  const urlHtml = await fetchPageHtml(url);
-  const $page = cherio.load(urlHtml);
+function parseOrderValue($el: Cheerio<Element>): string {
+  const val = $el.attr('order');
+  if (!val) {
+    throw new Error('Invalid column data');
+  } else {
+    return val;
+  }
+}
 
-  if ($page('#items-search').is('form')) {
+async function parseItemPage(
+  itemId: number,
+  serverId: number
+): Promise<IParseItemInfo> {
+  const url = `http://${DATASOURCE_HOSTNAME}/?c=market&a=item&id=${itemId}&setworld=${serverId}`;
+  const urlHtml = await fetchPageHtml(url);
+  const $ = cherio.load(urlHtml);
+
+  if ($('#items-search').is('form')) {
     throw new Error(`Invalid ID for ${url}`);
   }
 
-  const title = $page('#content h1').text();
-  const titleSup = $page('#content h1 sup').text();
+  const title = $('#content h1').text();
+  const titleSup = $('#content h1 sup').text();
+
+  const columnNames = $('#group_sell thead tr th')
+    .toArray()
+    .map(e => $(e).text().toLowerCase());
+  const findIndexByHeaderName = (name: string): number =>
+    columnNames.findIndex(val => val === name);
+  const colIndex = {
+    sellerName: findIndexByHeaderName('персонаж'),
+    sellerLocation: findIndexByHeaderName('город'),
+    price: findIndexByHeaderName('цена'),
+    amount: findIndexByHeaderName('кол-во'),
+    addedAt: findIndexByHeaderName('замечен'),
+    enchantmentLvl: findIndexByHeaderName('мод.')
+  };
+
+  const listings = $('#group_sell tbody tr')
+    .toArray()
+    .map(rowEl => {
+      const $rowEl = $(rowEl);
+      const rowClasses = $rowEl.attr('class');
+      if (!rowClasses) throw new Error('Invalid page!');
+
+      const id = rowClasses.split(' ')[0].replace('shop-', '');
+      const $cols = $(rowEl)
+        .find('td')
+        .toArray()
+        .map(el => $(el));
+
+      return {
+        id: parseInt(id, 10),
+        sellerName: $cols[colIndex.sellerName].text(),
+        sellerLocation: $cols[colIndex.sellerLocation].text(),
+        price: parseInt(parseOrderValue($cols[colIndex.price]), 10),
+        amount:
+          colIndex.amount >= 0
+            ? parseInt(parseOrderValue($cols[colIndex.amount]), 10)
+            : undefined,
+        addetAt: parseInt(parseOrderValue($cols[colIndex.addedAt]), 10) * 1000,
+        enchantmentLvl:
+          colIndex.enchantmentLvl >= 0
+            ? parseInt(parseOrderValue($cols[colIndex.enchantmentLvl]), 10)
+            : undefined
+      };
+    });
 
   return {
-    title: title.replace(titleSup, '').trim()
+    title: title.replace(titleSup, '').trim(),
+    listings
   };
 }
-
 export default parseItemPage;
