8415f90749fa9275219c075e7886d0fc1f429df0
Ruslan Savenok <ruslan.savenok@gmail.com>
2021-11-21 01:10:09 +0100

    initial listing

--- src/bot/listHandler.ts
+++ src/bot/listHandler.ts
@@ -1,5 +1,24 @@
 import { Composer } from 'grammy';
+import { table } from 'table';
+import ParseItemSubscription, {
+  IParseItemSubscription
+} from '../db/models/ParseItemSubscription';
 import { parseMessageData, isValidSubscription } from './utils';
+
+function renderResult(items: IParseItemSubscription[]) {
+  const result: any[] = [['Title', 'Price', 'Servers']];
+
+  items.forEach(item => {
+    const values = [
+      item.parseItem.title,
+      item.priceLimit.toLocaleString(),
+      item.servers.join(', ')
+    ];
+    result.push(values);
+  });
+
+  return `\`${table(result)}\``;
+}
 
 function handleListCommandFactory(command: string) {
   const bot = new Composer();
@@ -9,7 +28,11 @@ function handleListCommandFactory(command: string) {
 
     if (!isValidSubscription(ctx, user)) return;
 
-    ctx.reply('list');
+    const itemSubscriptions = await ParseItemSubscription.find({
+      tgUser: user
+    }).populate('parseItem');
+
+    ctx.reply(renderResult(itemSubscriptions), { parse_mode: 'Markdown' });
   });
 
   return bot;
