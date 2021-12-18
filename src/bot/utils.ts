a1d0b60e105347d0e5c582134805d24b7f534975
Ruslan Savenok <ruslan.savenok@gmail.com>
2021-12-18 19:34:25 +0100

    Fix slicing

--- src/bot/utils.ts
+++ src/bot/utils.ts
@@ -70,7 +70,7 @@ export function parseItemId(value: string): number {
 export function serverNameFromId(id: number): string {
   for (const [serverKey, serverId] of Object.entries(SERVERS)) {
     if (serverId === id)
-      return serverKey.charAt(0) + serverKey.toLowerCase().slice(0);
+      return serverKey.charAt(0) + serverKey.toLowerCase().slice(1);
   }
   return 'unknown';
 }
