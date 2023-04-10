85688c371cd5d5110d6ce238de2ed6e8baec91ee
Ruslan Savenok <ruslan.savenok@gmail.com>
2023-04-10 23:59:39 +0200

    API format and mongo connection

--- src/ui-api.ts
+++ src/ui-api.ts
@@ -1,6 +1,8 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyRequest } from 'fastify';
 import cors from '@fastify/cors';
-import { UI_API_PORT } from '../config';
+import { UI_API_PORT, MONGO_DB_URL, SERVERS } from '../config';
+import { setupConnection } from './db/connection';
+import logger from './logger';
 
 const fastify = Fastify({
   logger: true
@@ -9,13 +11,47 @@ fastify.register(cors, {
   origin: true
 });
 
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' });
-});
+const CHAR_REGEX_PATTERN = `^[^:]*\:(${Object.values(SERVERS).join('|')})$`;
 
-fastify.listen({ port: UI_API_PORT, host: '0.0.0.0' }, function (err) {
-  if (err) {
+fastify.get(
+  '/',
+  {
+    schema: {
+      querystring: {
+        type: 'object',
+        required: ['chars', 'accessKey'],
+        properties: {
+          accessKey: { type: 'string' },
+          chars: {
+            type: 'array',
+            items: {
+              type: 'string',
+              pattern: CHAR_REGEX_PATTERN
+            }
+          }
+        }
+      }
+    }
+  },
+  async function (
+    request: FastifyRequest<{
+      Querystring: { chars: string[]; accessKey: string };
+    }>,
+    reply
+  ) {
+    const characters = request.query.chars.map(val => val.split(':'));
+    reply.send(characters);
+  }
+);
+
+(async function () {
+  await setupConnection(MONGO_DB_URL);
+  logger.info('Mongo connected..');
+
+  try {
+    await fastify.listen({ port: UI_API_PORT, host: '0.0.0.0' });
+  } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-});
+})();
